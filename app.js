47793059957361125d5f02790f4e88d0d3084c1b
SimonGebru <te.gebru@hotmail.se>
2025-05-28 13:00:11 +0200

    Börjar om med frontenden efter att jag fått min backend att fungera felfritt

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
 const movieRoutes = require("./routes/movieRoutes");
 
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/users", userRoutes);
